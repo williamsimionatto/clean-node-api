e6a453cfc43f3c5bfc8f3ee7616e3aa481cf98ed
William <178344@upf.br>
2021-09-14 10:49:45 -0300

    refactor: change returns of method DbAddAccount.add

--- src/data/usecases/addaccount/db-add-account.ts
+++ src/data/usecases/addaccount/db-add-account.ts
@@ -13,6 +13,6 @@ export class DbAddAccount implements AddAccount {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
 
-    return await new Promise(resolve => resolve(account))
+    return account
   }
 }
