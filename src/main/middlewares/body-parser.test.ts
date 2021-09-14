8b0a3a5e6d25f6179ddfea266c5de569d1646e96
William <178344@upf.br>
2021-09-14 15:22:40 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'William' })
+      .expect({ name: 'William' })
+  })
+})
