3b762880514474fc93f57ab3f6c2d0b5d39648b3
William <178344@upf.br>
2021-09-20 12:55:20 -0300

    refactor: change tests

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('Body Parser Middleware', () => {
   test('Should parse body as json', async () => {
     app.post('/test_body_parser', (req, res) => {
-      res.send()
+      res.send('William')
     })
 
     await request(app)
