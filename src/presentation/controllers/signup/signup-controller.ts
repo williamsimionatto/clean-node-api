df8b970007c1816ccbaf95f3fb85109a3408c74c
William <178344@upf.br>
2021-10-08 14:59:42 -0300

    docs: update requirements signup

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -16,7 +16,7 @@ export class SignUpController implements Controller {
       }
 
       const { name, email, password } = httpRequest.body
-      const account = await this.addAccount.add({
+      await this.addAccount.add({
         name,
         email,
         password
