26dd9b78961726875a64b307c19034ba491d163c
William <178344@upf.br>
2021-10-08 13:04:25 -0300

    chore: update docker compose to use network

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -6,7 +6,7 @@ export const MongoHelper = {
 
   async connect (uri: string): Promise<void> {
     this.uri = uri
-    this.client = await MongoClient.connect(uri, {})
+    this.client = await MongoClient.connect(uri, { useUnifiedTopology: true })
   },
 
   async disconnect (): Promise<void> {
