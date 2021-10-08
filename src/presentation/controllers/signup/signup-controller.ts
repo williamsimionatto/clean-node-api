05bf7a2b0cb8739c22e10515dd5285622ac2a21e
William <178344@upf.br>
2021-10-08 14:50:09 -0300

    feat: esnure SignUpController calls Authentication with correct values

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,10 +1,11 @@
-import { HttpResponse, HttpRequest, Controller, AddAccount, Validation } from './signup-controller-protocols'
+import { HttpResponse, HttpRequest, Controller, AddAccount, Validation, Authentication } from './signup-controller-protocols'
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 
 export class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -21,6 +22,10 @@ export class SignUpController implements Controller {
         password
       })
 
+      await this.authentication.auth({
+        email,
+        password
+      })
       return ok(account)
     } catch (error) {
       return serverError(error)
