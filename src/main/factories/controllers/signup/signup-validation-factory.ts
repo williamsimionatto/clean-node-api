54e98e6bfc869fb64764ca0d3ee90ab31819cd1d
William <178344@upf.br>
2021-10-09 10:45:12 -0300

    refactor: move validation to its own layer

--- src/main/factories/controllers/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -1,6 +1,6 @@
-import { CompareFieldsValidation, RequiredFieldValidation, ValidationComposite, EmailValidation } from '../../../../presentation/helpers/validators'
+import { CompareFieldsValidation, RequiredFieldValidation, ValidationComposite, EmailValidation } from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../adapter/validators/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
