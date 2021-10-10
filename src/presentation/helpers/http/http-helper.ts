86fe026229661e034e8bf017e0341bb93cc9599c
William <178344@upf.br>
2021-10-10 10:10:25 -0300

    feat: esnure AddSurveyController retruns 204 on success

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,6 +1,21 @@
 import { ServerError, UnauthorizedError } from '../../errors'
 import { HttpResponse } from '../../protocols'
 
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
@@ -14,14 +29,4 @@ export const forbidden = (error: Error): HttpResponse => ({
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
-})
-
-export const ok = (data: any): HttpResponse => ({
-  statusCode: 200,
-  body: data
-})
-
-export const unauthorized = (): HttpResponse => ({
-  statusCode: 401,
-  body: new UnauthorizedError()
 })
