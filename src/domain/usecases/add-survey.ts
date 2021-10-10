e90384a1c8a9733a8e0ed1b3e8ee1a0bff1678be
William <178344@upf.br>
2021-10-10 10:00:30 -0300

    feat: esnure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface SurverAnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurveyModel {
+  question: string
+  answers: SurverAnswer[]
+}
+
+export interface AddSurvey {
+  add: (date: AddSurveyModel) => Promise<void>
+}
