72093cd7feea3bbc7c7ba2e2d4ec424d0c9c65ee
William <178344@upf.br>
2021-12-19 16:29:54 -0300

    feat: esnure LoadSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import { LoadSurveyById } from '../save-survey-result/save-survey-result-controller-protocols'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyResult } from './load-survey-result-controller-protocols'
 
@@ -17,8 +17,8 @@ export class LoadSurveyResultController implements Controller {
         return forbidden(new InvalidParamError('surveyId'))
       }
 
-      await this.loadSurveyResult.load(surveyId)
-      return null as any
+      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      return ok(surveyResult)
     } catch (error) {
       return await new Promise(resolve => resolve(serverError(error)))
     }
