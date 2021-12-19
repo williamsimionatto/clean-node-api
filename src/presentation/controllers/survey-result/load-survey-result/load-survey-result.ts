51b1649efa049c31a1f5b2a9dc9ba5e934a7020c
William <178344@upf.br>
2021-12-19 15:31:01 -0300

    feat: esnure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.ts
@@ -0,0 +1,13 @@
+import { LoadSurveyById } from '../save-survey-result/save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse } from './load-survey-result-controller-protocols'
+
+export class LoadSurveyResultController implements Controller {
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    return await new Promise(resolve => resolve(null as any))
+  }
+}
