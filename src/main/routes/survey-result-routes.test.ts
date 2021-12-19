98ace1f728bdb61ee7b74a2de70068dabd7571a9
William <178344@upf.br>
2021-12-19 18:01:38 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -55,7 +55,7 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    test('Should return 200 on save survey result without token', async () => {
+    test('Should return 200 on save survey result with token', async () => {
       const accessToken = await makeAccessToken()
       const res = await surveyCollection.insertOne({
         question: 'Question',
@@ -84,6 +84,26 @@ describe('Survey Routes', () => {
       await request(app)
         .get('/api/surveys/any_id/results')
         .expect(403)
+    })
+
+    test('Should return 200 on save survey result with token', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const id: string = res.ops[0]._id
+
+      await request(app)
+        .get(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
